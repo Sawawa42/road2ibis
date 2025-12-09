5604a602d56681d41ccb77bb1667e9354fe0eace
syamasaw <17191sawa@gmail.com>
2025-12-09 17:09:11 +0900

    wip

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,19 +1,64 @@
-#include "tinyPaint.hpp"
+#include <GLES3/gl32.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <vector>
 
-OpenGLFunctions gl;
+#include "Shader.hpp"
+#include "Mesh.hpp"
 
-int main(void) {
-    try {
-        TinyPaint app(400, 400, "TinyPaint");
-        if (app.init() != 0) {
-            std::cerr << "Failed to initialize TinyPaint" << std::endl;
-            return -1;
-        }
-        app.run();
-        app.cleanup();
-    } catch (const std::exception &e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-        return 1;
+const char* vertexShaderSource = R"(#version 300 es
+layout(location = 0) in vec3 aPos;
+layout(location = 1) in vec3 aColor;
+out vec3 ourColor;
+void main() {
+    gl_Position = vec4(aPos, 1.0);
+    ourColor = aColor;
+})";
+
+const char* fragmentShaderSource = R"(#version 300 es
+precision mediump float;
+in vec3 ourColor;
+out vec4 FragColor;
+void main() {
+    FragColor = vec4(ourColor, 1.0);
+})";
+
+int main() {
+    if (!glfwInit()) {
+        return -1;
     }
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "ex00", nullptr, nullptr);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    Shader shader(vertexShaderSource, fragmentShaderSource);
+
+    std::vector<float> vertices = {
+         0.0f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f, // 頂点1の座標xyzと色RGB
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f, // 頂点2の座標xyzと色RGB
+        -0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f // 頂点3の座標xyzと色RGB
+    };
+
+    Mesh mesh(vertices);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f); // 白色を背景色に設定
+        glClear(GL_COLOR_BUFFER_BIT); // 画面をクリア
+
+        shader.use();
+        mesh.draw();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
